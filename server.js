384d3ade265e03305ea0ad995f1828b1f5db15b5
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2018-01-16 14:40:41 -0500

    's'

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 8000;
 
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
-app.listen(443, function() {
+app.listen(port, function() {
   console.log('listening');
 });
