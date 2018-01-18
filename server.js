6c4cfef1fef6d11a7eac8113a4a999c1d89e3dc8
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2018-01-18 14:02:42 -0500

    's'

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ var port = process.env.PORT || 8000;
 
 app.use('/', express.static(path.join(__dirname + '/')));
 
-app.get('/resume', (req, res) => 
-		res.sendFile(path.join(__dirname, '../resume', 'resume.pdf'));
-	)
+app.post('/resume', function(req, res, next) {
+  var stream = fs.readStream('/resume');
+  var filename = "resume.pdf"; 
+  // Be careful of special characters
+
+  filename = encodeURIComponent(filename);
+  // Ideally this should strip them
+
+  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+  res.setHeader('Content-type', 'application/pdf');
+
+  stream.pipe(res);
+});
 
 app.listen(port, function() {
   console.log('listening');
