bca3653caff389db0656d46c6b1f14a826a7abe5
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2018-01-18 14:00:36 -0500

    's'

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var port = process.env.PORT || 8000;
 
 app.use('/', express.static(path.join(__dirname + '/')));
 
+app.get('/resume', (req, res) => 
+		res.sendFile(path.join(__dirname, '../resume', 'resume.pdf'));
+	)
+
 app.listen(port, function() {
   console.log('listening');
 });
