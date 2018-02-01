3ae4da3fefb69ad4c5b6ca3c5fd298b1a6ee9430
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2018-02-01 14:56:35 -0500

    trying to add stock buddy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var alpha = require('alphavantage')({key: '13H9GAWTQGC51OLB'});
 var express = require('express'),
     fs = require('fs'),
     app = express();
-require('.stockBuddy')(app);
+require('./stockBuddy')(app);
 	
 var port = process.env.PORT || 8000;
 
