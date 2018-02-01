64565e85a3e48c4a29bca7098ab70e9d0a70273d
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2018-01-31 23:05:55 -0500

    trying to add stock buddy

--- server.js
+++ server.js
@@ -16,6 +16,60 @@ app.get('/resume', function (req, res) {
     });
 });
 
+app.get('/stocks', function (req, res) {
+    function displaystuff(results,res){
+		var now = new Date();
+		var total = 0;
+		var idx = 0;
+	
+		for(var i in results){
+			var obj = results[i];
+	
+			var change = ((obj.value-obj.initvalue)*obj.amount - 9.99).toFixed(3);
+	
+			res.write(obj.name + ": " + change + "</br>");
+			total += parseFloat(change);
+			if (idx === results.length - 1){
+				res.write("</br>Total: " + total.toFixed(2));
+				res.end();
+			}
+			idx++;
+		}
+	}
+
+	function getTimeNow(){
+		var now = new Date();
+		var year = now.getFullYear();
+		var month = now.getMonth() + 1;
+		var date = now.getDate();
+		var hour = now.getHours();
+		var minute = now.getMinutes();
+		if(minute < 10){
+			minute = '0' + minute;
+		}
+		return year + "-" + month + "-" + date + " " + hour + ":" + minute;
+	}
+	
+	function get_quote(ticker) {
+		alpha.data.intraday(ticker).then((data) => {
+			var now = new Date();
+	
+			var dayobj = data['Time Series (1min)'];
+	
+			for (var props in dayobj) {
+				var time = props;
+				break;
+			}
+	
+			var timeobj = dayobj[time];
+			var curPrice = timeobj['4. close'];
+		}).catch(function () {
+			console.log("Promise Rejected for " + ticker);
+		});
+	}
+
+});
+
 app.listen(port, function() {
   console.log('listening');
 });
