79de5cac33b1a6179de8f43dc64975891955463e
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2019-12-10 00:30:15 -0500

    mouse hovering over fire is cool

--- public/games/flame/scripts/main.js
+++ public/games/flame/scripts/main.js
@@ -1,16 +1,36 @@
 let canvas = document.getElementById('gameCanvas');
 let ctx;
-let flameWidth = 60;
+let flameWidth = 50;
+let mouseLoc = {
+	x: null,
+	y: null
+}
 
-let n = new Array(204);
-let m = new Array(204);
+let size = 204;
+
+let n = new Array(size);
+let m = new Array(size);
 for (let i = 0; i < n.length; i++) {
-	n[i] = new Array(202);
-	m[i] = new Array(202);
+	n[i] = new Array(size);
+	m[i] = new Array(size);
 	for (let j = 0; j < n.length; j++) {
 		n[i][j] = (j > n.length - 3 && i > 100 - flameWidth && i < 100 + flameWidth) ? 255 : 0;
 		m[i][j] = (j > n.length - 3 && i > 100 - flameWidth && i < 100 + flameWidth) ? 255 : 0;
 	}
+}
+
+canvas.addEventListener('mousemove', function(e) {
+  // important: correct mouse position:
+  let rect = this.getBoundingClientRect();
+	let x = e.clientX - rect.left;
+	let y = e.clientY - rect.top;
+	mouseLoc.x = Math.floor(size * (x / canvas.width));
+	mouseLoc.y = Math.floor(size * (y / canvas.height));
+});
+
+canvas.onmouseout = function(e) {
+	mouseLoc.x = null;
+	mouseLoc.y = null;
 }
 
 window.onload = (() => {
@@ -24,7 +44,21 @@ window.onload = (() => {
 	}, 20);
 });
 
-function movePixels(){
+function movePixels() {
+	if (mouseLoc.x && mouseLoc.y) {
+		let mouseTemp = 0;
+		try {
+			m[mouseLoc.x][mouseLoc.y] = mouseTemp;
+			m[mouseLoc.x - 1][mouseLoc.y - 1] = mouseTemp;
+			m[mouseLoc.x][mouseLoc.y - 1] = mouseTemp;
+			m[mouseLoc.x + 1][mouseLoc.y - 1] = mouseTemp;
+			m[mouseLoc.x + 1][mouseLoc.y] = mouseTemp;
+			m[mouseLoc.x + 1][mouseLoc.y + 1] = mouseTemp;
+			m[mouseLoc.x][mouseLoc.y + 1] = mouseTemp;
+			m[mouseLoc.x - 1][mouseLoc.y + 1] = mouseTemp;
+			m[mouseLoc.x - 1][mouseLoc.y] = mouseTemp;
+		} catch (e) {}
+	}
 	for (let i = 1; i < n.length - 1; i++) {
 		for (let j = 1; j < n.length - 2; j++) {
 			n[i][j] = (m[i][j] + 0.5*m[i-1][j] + 0.5*m[i+1][j] + 0.5*m[i][j-1] + 10*m[i][j+1] + 1*m[i][j+2]) / 13.5;
@@ -54,7 +88,7 @@ function heatToColor(heat) {
 	let r = heat;
 	let g = heat-70;
 	let b = (heat - 150) * 1.66;
-	
+
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
--- server.js
+++ server.js
@@ -6,15 +6,15 @@ let express = require('express'),
 let port = process.env.PORT || 8000;
 
 app.use(express.static('public')); //Serves resources from public folder
-//
-// app.get('/resume', function (req, res) {
-//     var filePath = "/resume/resume.pdf";
-//
-//     fs.readFile(__dirname + filePath , function (err,data){
-//         res.contentType("application/pdf");
-//         res.send(data);
-//     });
-// });
+
+app.get('/resume', function (req, res) {
+    var filePath = "/resume/resume.pdf";
+
+    fs.readFile(__dirname + filePath , function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+});
 
 
 app.listen(port, function() {
