015d79aa9bc65d4ad2e462e4757811c32492b950
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2019-12-09 22:22:09 -0500

    moved all web stuff to one directory

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var path = require('path');
-var express = require('express'),
+let path = require('path');
+let express = require('express'),
     fs = require('fs'),
     app = express();
-require('./stockBuddy/stockBuddy')(app);
-	
-var port = process.env.PORT || 8000;
 
-app.get('/resume', function (req, res) {
-    var filePath = "/resume/resume.pdf";
+let port = process.env.PORT || 8000;
 
-    fs.readFile(__dirname + filePath , function (err,data){
-        res.contentType("application/pdf");
-        res.send(data);
-    });
-});
+app.use(express.static('public')); //Serves resources from public folder
+//
+// app.get('/resume', function (req, res) {
+//     var filePath = "/resume/resume.pdf";
+//
+//     fs.readFile(__dirname + filePath , function (err,data){
+//         res.contentType("application/pdf");
+//         res.send(data);
+//     });
+// });
 
-app.use('/', express.static(path.join(__dirname + '/')));
 
 app.listen(port, function() {
   console.log('listening on port ' + port);
